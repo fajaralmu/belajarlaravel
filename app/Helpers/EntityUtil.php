cc583ae437ac747733f709b78bc017063fa47216
Fajar AM <somabangsa@gmail.com>
2020-08-16 17:37:04 +0700

    update entity util

--- app/Helpers/EntityUtil.php
+++ app/Helpers/EntityUtil.php
@@ -2,6 +2,43 @@
 
 namespace App\Helpers;
 
+use App\Annotations\FormField;
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use  Doctrine\Common\Annotations\AnnotationReader;
+use ReflectionProperty;
+use ReflectionClass;
+
 class EntityUtil {
     
+
+    public static function getAnnotations(ReflectionClass  $reflectionClass ){
+        AnnotationRegistry::registerLoader('class_exists');
+ 
+        $properties = $reflectionClass->getProperties();
+        $annotations = array();
+
+        foreach($properties as $property){
+              
+            $myAnnotation = EntityUtil::getPropertyAnnotation($property, FormField::class );
+
+            if(!is_null( $myAnnotation)){
+                $myAnnotation->fieldName = $property->getName();
+                array_push($annotations, $myAnnotation);
+            }
+           
+        }
+
+        return $annotations;
+    }
+
+    public static function getPropertyAnnotation(ReflectionProperty $property, $annotationClass){
+        $reader = new AnnotationReader();
+        $myAnnotation = $reader->getPropertyAnnotation(
+            $property,
+            $annotationClass
+        );
+        return $myAnnotation;
+    }
+
 }
--- app/Services/WebConfigService.php
+++ app/Services/WebConfigService.php
@@ -14,12 +14,7 @@ use App\Models\RegisteredRequest;
 use App\Models\ScheduledFoodTaskGroup;
 use App\User;
 use ReflectionClass;  
-
-//
-use App\Annotations\FormField; 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use  Doctrine\Common\Annotations\AnnotationReader;
-use ReflectionProperty;
+use App\Helpers\EntityUtil; 
 
 class WebConfigService {
 
@@ -39,44 +34,15 @@ class WebConfigService {
         AppProfile::class, Member::class, MemberFee::class, FoodTaskGroup::class,
         FoodTaskGroupMember::class, RegisteredRequest::class, ScheduledFoodTaskGroup::class);
 
-    }
-
-    private function getAnnotations(ReflectionClass  $reflectionClass ){
-        AnnotationRegistry::registerLoader('class_exists');
- 
-        $properties = $reflectionClass->getProperties();
-        $annotations = array();
-
-        foreach($properties as $property){
-              
-            $myAnnotation = $this->getProperyAnnotation($property, FormField::class );
-
-            if(!is_null( $myAnnotation)){
-                $myAnnotation->fieldName = $property->getName();
-                array_push($annotations, $myAnnotation);
-            }
-           
-        }
-
-        return $annotations;
-    }
-
-    public function getProperyAnnotation(ReflectionProperty $property, $annotationClass){
-        $reader = new AnnotationReader();
-        $myAnnotation = $reader->getPropertyAnnotation(
-            $property,
-            $annotationClass
-        );
-        return $myAnnotation;
-    }
-
+    } 
+    
     public function getModelInfos(string $model_code){
        
         if(!array_has( $this->entityConfig, $model_code)){
             return [];
         }
         $refectionClass = $this->entityConfig[$model_code]; 
-        return $this->getAnnotations($refectionClass);
+        return EntityUtil::getAnnotations($refectionClass);
 
     }
 
