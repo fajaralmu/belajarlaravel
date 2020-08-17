a4891cd21dc8de5160e98296c18c649e6bcfb995
Fajar AM <somabangsa@gmail.com>
2020-08-17 12:47:52 +0700

    refactor

--- app/Repositories/EntityRepository.php
+++ app/Repositories/EntityRepository.php
@@ -1,7 +1,72 @@
 <?php 
-namespace App\Repositories; 
+namespace App\Repositories;
+
+use App\Dto\Filter;
+use Illuminate\Support\Facades\DB;
+use ReflectionClass;
 
 class EntityRepository {
      
+
+    function getTableName (ReflectionClass $reflectionClass){
+        $props = $reflectionClass->getDefaultProperties();
+        
+        // dd(json_encode($props));
+        foreach($props as$key=> $prop) {
+            if( $key == "table"){
+                return $prop;
+            }
+        }
+        
+        return null;
+    }
+
+    public function filter(ReflectionClass $reflectionClass, Filter $filter){
+        $tableName = $this->getTableName($reflectionClass);
+        
+        $db = DB::table($tableName);//->get()->toArray();
+        $db_count = DB::table($tableName);
+        $whereClause = [];
+        
+       
+        if( isset($filter)){
+            $filter =  $filter;
+            $fieldsFilter = $filter->fieldsFilter;
+            $exactSearch = $filter->exacts;
+            
+            if(isset( $fieldsFilter) && sizeof( $fieldsFilter) > 0){
+                foreach( $fieldsFilter as $key=>$value){
+                    $operator = null;
+                     
+                    if($exactSearch){
+                        $operator = "like";
+                        $value = '%'.$value.'%';
+                    }else{
+                        $operator = "=";
+                    }
+                    array_push($whereClause, [$key, $operator, $value]);
+                }
+
+               $db = $db->where($whereClause);
+               $db_count = $db_count->where($whereClause);
+            } 
+            if(isset($filter->limit) && $filter->limit>0){
+                $offset = $filter->page * $filter->limit;
+                $db = $db->offset($offset)->limit($filter->limit);
+            }
+            if(isset($filter->orderBy) && trim($filter->orderBy)!=""){
+                $orderType = strtolower($filter->orderType) == "desc"?"asc":strtolower($filter->orderType);
+                $db = $db->orderBy($filter->orderBy, $orderType);
+            }
+         }
+         
+        $resultList = $db->get()->toArray();
+        $count = $db_count->count();
+
+        return [
+            "resultList"=>$resultList,
+            "count"=>$count
+        ];
+    }
     
 }
--- app/Services/EntityService.php
+++ app/Services/EntityService.php
@@ -22,69 +22,19 @@ class EntityService {
     private function getEntityConfig(string $entityName){ 
         $result = $this->webConfigService->getConfig($entityName);
         return $result;
-    }
-    function getTableName (ReflectionClass $reflectionClass){
-        $props = $reflectionClass->getDefaultProperties();
-        
-        // dd(json_encode($props));
-        foreach($props as$key=> $prop) {
-            if( $key == "table"){
-                return $prop;
-            }
-        }
-        
-        return null;
-    }
+    } 
 
     public function filter(WebRequest $webRequest){
         $reflectionClass = $this->getEntityConfig($webRequest->entity);
         if(is_null( $reflectionClass )){
             return WebResponse::failed("Invalid request");
         }
-        $tableName = $this->getTableName($reflectionClass);
-        
-        $db = DB::table($tableName);//->get()->toArray();
-        $db_count = DB::table($tableName);
-        $whereClause = [];
-        
        
-        if(property_exists($webRequest, "filter") && isset($webRequest->filter)){
-            $filter =  $webRequest->filter;
-            $fieldsFilter = $filter->fieldsFilter;
-            $exactSearch = $filter->exacts;
-            
-            if(isset( $fieldsFilter) && sizeof( $fieldsFilter) > 0){
-                foreach( $fieldsFilter as $key=>$value){
-                    $operator = null;
-                     
-                    if($exactSearch){
-                        $operator = "like";
-                        $value = '%'.$value.'%';
-                    }else{
-                        $operator = "=";
-                    }
-                    array_push($whereClause, [$key, $operator, $value]);
-                }
-
-               $db = $db->where($whereClause);
-               $db_count = $db_count->where($whereClause);
-            } 
-            if(isset($filter->limit) && $filter->limit>0){
-                $offset = $filter->page * $filter->limit;
-                $db = $db->offset($offset)->limit($filter->limit);
-            }
-            if(isset($filter->orderBy) && trim($filter->orderBy)!=""){
-                $orderType = strtolower($filter->orderType) == "desc"?"asc":strtolower($filter->orderType);
-                $db = $db->orderBy($filter->orderBy, $orderType);
-            }
-         }
-         
-        $resultList = $db->get()->toArray();
-        $count = $db_count->count();
+        $result = $this->entityRepository->filter($reflectionClass, $webRequest->filter);
 
         $response = new WebResponse(); 
-        $response ->entities = $resultList;
-        $response ->totalData =  $count;
+        $response ->entities = $result["resultList"];
+        $response ->totalData = $result["count"];
         $response ->filter = $webRequest->filter;
         return $response;
     }
