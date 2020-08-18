a397a434ff8bce04e9d55a65c596cd8c1bb2fdc9
Fajar AM <somabangsa@gmail.com>
2020-08-18 09:14:48 +0700

    fixing update image

--- app/Services/EntityService.php
+++ app/Services/EntityService.php
@@ -8,10 +8,11 @@ use App\Dto\WebResponse;
 use App\Helpers\EntityUtil;
 use App\Helpers\FileUtil;
 use App\Models\BaseModel;
-use App\Repositories\EntityRepository; 
+use App\Repositories\EntityRepository;
 use ReflectionClass;
 
-class EntityService {
+class EntityService
+{
 
     private EntityRepository $entityRepository;
     private WebConfigService $webConfigService;
@@ -22,155 +23,193 @@ class EntityService {
         $this->webConfigService = $webConfigService;
     }
 
-    private function getEntityConfig(string $entityName){ 
-        $result = $this->webConfigService->getConfig($entityName);
+    private function getEntityConfig(string $entityName)
+    {
+        $result = $this
+            ->webConfigService
+            ->getConfig($entityName);
         return $result;
-    } 
+    }
 
-    private function validateEntityValuesBeforePersist($entity){
+    private function validateEntityValuesBeforePersist($entity, $originalObject = null)
+    {
         $class = new ReflectionClass($entity);
         $props = $class->getProperties();
         // $joinColumns = $this->webConfigService->getJoinColumns(new ReflectionClass($entity));
-        foreach ($props as $prop) {
+        foreach ($props as $prop)
+        {
 
             $propName = $prop->name;
-            $propValue = $entity->$propName;  
+            $propValue = $entity->$propName;
             $formField = EntityUtil::getPropertyAnnotation($prop, FormField::class);
-            if(is_null($formField) || !isset($propValue)){
+            if (is_null($formField)  )
+            {
                 continue;
             }
-             
-            if( $formField->foreignKey != "" ){
+
+            if ($formField->foreignKey != "" && isset($propValue))
+            {
                 $formField = EntityUtil::getPropertyAnnotation($prop, FormField::class);
                 $foreignKey = $formField->foreignKey;
-                $entity->$foreignKey =  $propValue->id;
+                $entity->$foreignKey = $propValue->id;
                 unset($entity->$propName);
-            }else if($formField->type == "FIELD_TYPE_IMAGE"){
-                $imgName = FileUtil::writeBase64File($propValue, "IMG_".rand(1,999));
-                $entity->$propName = $imgName;
             }
-            
+            else if ($formField->type == "FIELD_TYPE_IMAGE")
+            {
+                if(isset($propValue) || trim($propValue != "")){
+                    $imgName = FileUtil::writeBase64File($propValue, "IMG_" . rand(1, 999));
+                    $entity->$propName = $imgName;
+                }else  if(!is_null($originalObject)){
+                    $entity->$propName = $originalObject->$propName;
+                }
+
+                
+            }
+
         }
         return ($entity);
     }
 
-    private function validateEntityValuesAfterFilter(ReflectionClass $class, object $entity, bool $validateJoinColumn){
-         
+    private function validateEntityValuesAfterFilter(ReflectionClass $class, object $entity, bool $validateJoinColumn)
+    {
+
         $props = $class->getProperties();
-        
-        foreach($props as $prop){
+
+        foreach ($props as $prop)
+        {
             $propName = $prop->name;
             $formField = EntityUtil::getPropertyAnnotation($prop, FormField::class);
-            if(is_null($formField)){
+            if (is_null($formField))
+            {
                 continue;
             }
-            
-            if( $formField->foreignKey != "" && $validateJoinColumn == true){ 
-                 /**
-                  * join columns
-                  */ 
-                  $foreignKey = $formField->foreignKey; 
-                  $propValue =  $entity->$foreignKey; 
-                  
-                  $referenceClass = new ReflectionClass( $formField->className); 
-                  $referenceObject = $this->entityRepository->findById($referenceClass, $propValue ); 
-                  
-                  $entity->$propName = $referenceObject; 
-            }else{
+
+            if ($formField->foreignKey != "" && $validateJoinColumn == true)
+            {
+                /**
+                 * join columns
+                 */
+                $foreignKey = $formField->foreignKey;
+                $propValue = $entity->$foreignKey;
+
+                $referenceClass = new ReflectionClass($formField->className);
+                $referenceObject = $this
+                    ->entityRepository
+                    ->findById($referenceClass, $propValue);
+
+                $entity->$propName = $referenceObject;
+            }
+            else
+            {
                 /**
                  * regular
                  */
                 $propValue = $entity->$propName;
-                if((is_null($propValue) || $propValue == "") && $formField->defaultValue != ""){
+                if ((is_null($propValue) || $propValue == "") && $formField->defaultValue != "")
+                {
                     $entity->$propName = $formField->defaultValue;
                 }
-
-
             }
         }
-        
-        // foreach ($joinColumns as $prop) {
-        //     $propName = $prop->name; 
-        //     $formField = EntityUtil::getPropertyAnnotation($prop, FormField::class);
-        //     $foreignKey = $formField->foreignKey; 
-        //     $propValue =  $entity->$foreignKey; 
-            
-        //     $referenceClass = new ReflectionClass( $formField->className); 
-        //     $referenceObject = $this->entityRepository->findById($referenceClass, $propValue ); 
-            
-        //     $entity->$propName = $referenceObject; 
-        // }
-          
-        return $entity;  
-    } 
 
-    public function filter(WebRequest $webRequest){
-        $entityCode = ($webRequest->entity);
-       
-        return $this->doFilter($entityCode,  $webRequest->filter);
+        // foreach ($joinColumns as $prop) {
+        //     $propName = $prop->name;
+        //     $formField = EntityUtil::getPropertyAnnotation($prop, FormField::class);
+        //     $foreignKey = $formField->foreignKey;
+        //     $propValue =  $entity->$foreignKey;
+        //     $referenceClass = new ReflectionClass( $formField->className);
+        //     $referenceObject = $this->entityRepository->findById($referenceClass, $propValue );
+        //     $entity->$propName = $referenceObject;
+        // }
+        return $entity;
     }
 
-    public function doFilter(string $entityCode, Filter $filter){
+    public function filter(WebRequest $webRequest)
+    {
+        $entityCode = ($webRequest->entity);
+
+        return $this->doFilter($entityCode, $webRequest->filter);
+    }
+
+    public function doFilter(string $entityCode, Filter $filter)
+    {
         $reflectionClass = $this->getEntityConfig($entityCode);
-        if(is_null( $reflectionClass )){
+        if (is_null($reflectionClass))
+        {
             return WebResponse::failed("Invalid request");
         }
-       
-        $result = $this->entityRepository->filter($reflectionClass, $filter);
-        $resultList =  $result["resultList"];
+
+        $result = $this
+            ->entityRepository
+            ->filter($reflectionClass, $filter);
+        $resultList = $result["resultList"];
         $validated = [];
-        foreach ($resultList as $res) {
-           array_push($validated, $this->validateEntityValuesAfterFilter( $reflectionClass, $res, true));
+        foreach ($resultList as $res)
+        {
+            array_push($validated, $this->validateEntityValuesAfterFilter($reflectionClass, $res, true));
         }
-        
-        $response = new WebResponse(); 
-        $response->entities =  $validated;
+
+        $response = new WebResponse();
+        $response->entities = $validated;
         $response->totalData = $result["count"];
         $response->filter = $filter;
         return $response;
     }
-    
 
-    public function add(WebRequest $webRequest){
+    public function add(WebRequest $webRequest)
+    {
         $entityCode = ($webRequest->entity);
-        $entityObject = $webRequest->$entityCode; 
-        return $this->doAdd($entityCode,  $entityObject);
+        $entityObject = $webRequest->$entityCode;
+        return $this->doAdd($entityCode, $entityObject);
     }
 
-    public function doAdd(string $entityCode, BaseModel $entityObject){
+    public function doAdd(string $entityCode, BaseModel $entityObject)
+    {
         $reflectionClass = $this->getEntityConfig($entityCode);
-        if(is_null( $reflectionClass )){
-            return WebResponse::failed("Invalid request");
+        if (is_null($reflectionClass))
+        {
+            return  WebResponse::failed("Invalid request");
         }
- 
+
         $validatedObj = $this->validateEntityValuesBeforePersist($entityObject);
-        unset($validatedObj->id);  
+        unset($validatedObj->id);
         $validatedObj->save();
 
-        $response = new WebResponse(); 
+        $response = new WebResponse();
         // $response->entity =  $entityObject;
         return $response;
     }
 
-    public function update(WebRequest $webRequest){
+    public function update(WebRequest $webRequest)
+    {
         $entityCode = ($webRequest->entity);
-        $entityObject = $webRequest->$entityCode; 
-        return $this->doUpdate($entityCode,  $entityObject);
+        $entityObject = $webRequest->$entityCode;
+        return $this->doUpdate($entityCode, $entityObject);
     }
 
-    public function doUpdate(string $entityCode, BaseModel $entityObject){
+    public function doUpdate(string $entityCode, BaseModel $entityObject)
+    {
         $reflectionClass = $this->getEntityConfig($entityCode);
-       
-        if(is_null($reflectionClass )){
+
+        if (is_null($reflectionClass))
+        {
             return WebResponse::failed("Invalid request");
         }
-        
-        $validatedObj = $this->validateEntityValuesBeforePersist($entityObject);
-        
-        $result = $this->entityRepository->update($reflectionClass,  $validatedObj);  
+        $originalObject = $this->entityRepository->findById( $reflectionClass, $entityObject->id);
+
+        if(is_null( $originalObject)){
+            return WebResponse::failed("Original Objet Does Not Exist");
+        }
+
+        $validatedObj = $this->validateEntityValuesBeforePersist($entityObject, $originalObject );
+
+        $result = $this
+            ->entityRepository
+            ->update($reflectionClass, $validatedObj);
         // dd($queries);
-        $response = new WebResponse(); 
-         $response->message =  $result;
+        $response = new WebResponse();
+        $response->message = $result;
         return $response;
     }
 }
+
