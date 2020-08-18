834e1efa798f6e7b2be699031bb9beabbec832ef
Fajar AM <somabangsa@gmail.com>
2020-08-18 14:00:06 +0700

    update error page

--- resources/views/error/index.blade.php
+++ resources/views/error/index.blade.php
@@ -4,8 +4,10 @@
 </head>
 <body>
 <div class="content">
-	<h2>Sorry! Error Caught</h2>
-	<p>{{$message}}</p>
+	<h2><u>Internal Error</u></h2>
+	<h4>Message:</h4>
+	<p>{{$message}}</p> 
+	<span>Please contact the administrator for further actions</span>
 </div>
 </body>
 </html>
